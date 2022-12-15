8528601e6d188e9fc03e2c5ca7f5f752a8efd903
Name Surname <StudentID@newuzbekistanuniversity.uz>
2022-12-15 12:44:02 +0500

    commited succesfully

--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
-        if ((firstSide*firstSide)+(secondSide*secondSide)==thirdSide*thirdSide){
+        if ((Math.pow(firstSide,2))*(Math.pow(secondSide,2))==Math.pow(thirdSide,2)){
             System.out.println("this is a valid triangle");
         }else{
             System.out.println("it's not a triangle");
